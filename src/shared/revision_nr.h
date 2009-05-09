13eac83a0a9fba0d232a42b5e0752c43c17306f8
Bazilio <BazilioRn@gmail.com>
2009-05-09 22:34:15 +0400

    [22] Some update of spell target selection.

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -18,7 +18,7 @@
  
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "21"
+ #define REVISION_NR "22"
 #endif // __REVISION_NR_H__
 #ifndef __REVISION_DR_H__
 #define REVISION_DR "9.05.2009"
